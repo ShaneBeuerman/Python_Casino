1c3065ae2fe68fb987c27fde183269b86fd07d28
ShaneBeuerman <shanebeuerman@gmail.com>
2021-05-10 14:34:38 -0600

    Updated Blackjack.py
    Updated Blackjack.py to function in the correct order and to detect if a player wins or loses.

--- Blackjack.py
+++ Blackjack.py
@@ -9,22 +9,43 @@ class Blackjack:
     handValue = 0
     dealerValue = 0
 
-    def hitOrStay(self):
-        handValue = hit(hand)
-        dealerValue = hit(dealer)
-        choice = input("Would you like to hit or stay?")
+    def play(self, wager):
+        self.handValue = hit(hand)
+        self.dealerValue = hit(dealer)
+        self.displayHand(hand)
+        self.displayHand(dealer)
+        if self.handValue == 21:
+            print("You win")
+            return wager * 2
+        elif self.dealerValue == 21:
+            print("You lose")
+            return 0
+        hitOrStand()
+        if busted(self.handValue):
+            print("You lose")
+            return 0
+        dealerPlay()
+        compare()
+
+    def hitOrStand(self):
+        choice = input("Would you like to hit or stand? Type 'hit' to hit and 'stand' to stand")
+        while(choice != 'hit' or choice != 'stand'):
+            print("Please type in either 'hit' or 'stand'")
+            choice = input("Would you like to hit or stand?")
         if choice == 'hit':
             print("You decided to hit")
-        elif choice == 'stay':
-            print("You have decided to stay")
-        else:
-            print("Sorry, but you have not given a satisfactory response.")
-            print("Please type in either 'hit' or 'stay'")
-            hitOrStay()
+            hit(self.hand)
+            displayHand(self.hand)
+            if busted(self.hand):
+                print("You lose")
+                return
+            hitOrStand()
+        elif choice == 'stand':
+            print("You have decided to stand")
 
-    def displayHand(self):
+    def displayHand(self, curHand):
         value = 0
-        for card in hand:
+        for card in curHand:
             if card.value == 1:
                 print("Ace", card.suit)
             elif card.value == 11:
@@ -35,20 +56,15 @@ class Blackjack:
                 print("King", card.suit)
             else:
                 print(card.value, card.suit)
+            value += card.value
+        print(value)
 
     def hit(self, curHand):
         if not curHand:
-            #hand draws twice
-            displayHand()
+            curHand.append(deck.pop(0))
+            curHand.append(deck.pop(0))
         else:
-            #hand draws once
-            displayHand()
-            #check if busted
-            #if busted:
-                #Game ends
-            #else:
-                #Hit or stay?
-        print("To be continued")
+            curHand.append(deck.pop(0))
 
     def busted(self, val):
         if val > 21:
@@ -57,20 +73,32 @@ class Blackjack:
             return False
 
     def dealerPlay(self):
-        print("Not yet ready")
+        if dealerValue > 16:
+            print("Dealer stands")
+            return
+        if dealerValue > handValue:
+            print("Dealer has no need to hit.")
+            return
+        hit(dealer)
+        if dealerValue > 21:
+            print("Dealer busts")
+            return
+        dealerPlay()
 
     def compare(self):
-        if handValue <= 21 and dealerValue <= 21:
-            if handValue > dealerValue:
+        if self.handValue <= 21 and self.dealerValue <= 21:
+            if self.handValue > self.dealerValue:
                 print("You win")
-                #payout
-            elif dealerValue > handValue:
+                return "win"
+            elif self.dealerValue > self.handValue:
                 print("You lose")
-                #no payout
+                return "lose"
             else:
                 print("Tie")
-                #pay back your wager
-        elif handValue > 21:
+                return "tie"
+        elif busted(self.handValue):
             print("You lose")
-        elif dealerValue > 21:
-            print("You lose")
+            return "lose"
+        elif busted(self.dealerValue):
+            print("You win")
+            return "win"
